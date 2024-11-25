85cacda5901a177ba9a9e5d69ca6181b4092a24f
Yuli <yu.expert@gmail.com>
2024-11-25 12:40:41 +0200

    Add component Description

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,13 @@
+const Description = () => {
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <div>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </div>
+    </div>
+  );
+};
+
+export default Description;
